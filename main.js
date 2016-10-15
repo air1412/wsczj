7eb7dc2972d56a31aee40ab2635c92b13ed74086
air1412 <240312645@qq.com>
2016-10-15 16:12:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function $(id){
+	return document.getElementById(id);
+}
+
+
+
+window.onload = function(){
+	var n = 0;
+	var firstpage = $('m-fp');
+	var times = setInterval(time,1000);
+	
+	function time(){
+		if(n == 5){firstpage.style.display = 'none';clearInterval(times);}
+		var date = new Date();
+		var nowdate = formatDate(date);
+		var timer = $('m-fp').getElementsByTagName('span');
+		timer[0].innerHTML = "将于 "+ (5-n) + " 秒后进入主页　|　"+ nowdate ;
+		n++;
+	}time();
+}
+
+
+ function padding(number){
+        return number < 10 ? '0' + number : '' + number;
+}
+function formatDate(date){
+	return date.getFullYear() + '-' 
+		+ padding(date.getMonth() + 1) + '-' 
+		+ padding(date.getDate()) + ' ' 
+		+ padding(date.getHours()) + ':'
+		+ padding(date.getMinutes()) + ':'
+		+ padding(date.getSeconds());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
