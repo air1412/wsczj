3ecfdef9aea1fd3dbd830fff78e749699c49e8bc
air1412 <240312645@qq.com>
2016-10-25 19:40:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,20 +6,42 @@ function $(id){
 
 window.onload = function(){
 	var n = 0;
-	var firstpage = $('m-fp');
+	var timer = $('m-top').getElementsByTagName('span');
 	var times = setInterval(time,1000);
-	var inter = firstpage.getElementsByTagName('a');
-	inter[0].onclick = function(){firstpage.style.display = 'none';fadeOut(firstpage,800);clearInterval(times);}
 	function time(){
-		//if(n == 5){firstpage.style.display = 'none';fadeOut(firstpage,800);clearInterval(times);}
 		var date = new Date();
 		var nowdate = formatDate(date);
-		var timer = $('m-fp').getElementsByTagName('span');
-		timer[0].innerHTML = nowdate;
-		inter[0].innerHTML = (5-n) + " 秒后进入 < 主页 >";
+		timer[1].innerHTML = "Now is " + date.getFullYear();
+		timer[2].innerHTML = padding(date.getMonth()+1);
+		timer[3].innerHTML = padding(date.getDate());
+		timer[4].innerHTML = padding(date.getHours());
+		timer[5].innerHTML = padding(date.getMinutes());
+		timer[6].innerHTML = padding(date.getSeconds());
+		fadeIn(timer[6],300);
 		n++;
 	}time();
 }
+
+function scrollit() {
+	var i = 1;
+	var autoccroll = setInterval(function(){
+		if (i<=600) {
+			scroll(1,i);
+			i += 5;
+		}else if(i<=700){
+			scroll(1,i);
+			i += 3;
+		}else if(i<=800){
+			scroll(1,i);
+			i++;
+		}else{
+			clearInterval(autoccroll);
+		}
+	},1);
+
+
+}
+
 
 
  function padding(number){
@@ -39,7 +61,6 @@ function formatDate(date){
 function fadeIn(obj,time){//淡入函数  实现time毫秒后显示，原理是根据透明度来完成的
 	var startTime = new Date(); 
 	obj.style.opacity = 0;//设置下初始值透明度
-	obj.style.display= 'block';
 	var timer = setInterval(function(){
 		var nowTime = new Date();
 		var prop = (nowTime-startTime)/time;
